84863eb2b6cf68beecd0ee7b1ff4e705721f6b01
saad <saadsha201@gmail.com>
2021-09-23 08:52:48 -0700

    Update inshackle.sh

--- inshackle.sh
+++ inshackle.sh
@@ -452,7 +452,7 @@ sleep 3
 
 done
 printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 5 min...\e[0m\n"
-sleep 300
+sleep 600
 #unfollow
 for celeb in $(cat celeb_id); do
 data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
@@ -470,7 +470,7 @@ fi
 sleep 3
 done
 printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 5 min...\e[0m\n"
-sleep 300
+sleep 600
 
 
 done
